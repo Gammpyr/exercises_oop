2abea49e90ceb2a0b7f74924924299c66b16e377
Gammpyr <malak93@yandex.ru>
2025-05-27 13:26:13 +0300

    solved tasks

--- lesson1/number.py
+++ lesson1/number.py
@@ -15,16 +15,16 @@
 class Number:
 
     def __init__(self, value):
-        ...
+        self.value = value
 
-    def ...(self):
-        ...
+    def get(self):
+        return self.value
 
-    def ...:
-        ...
+    def add(self, add_v):
+        self.value += add_v
 
-    def ...:
-        ...
+    def substract(self, subtr_v):
+        self.value += subtr_v
 
 
 # код для проверки 
